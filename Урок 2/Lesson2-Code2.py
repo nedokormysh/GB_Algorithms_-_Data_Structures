dcdd7748368447ac3b4f8ed24bc5a8a313d4a8de
nedokormysh <67223378+nedokormysh@users.noreply.github.com>
2022-01-31 15:16:07 +0300

    Delete Урок 2 directory

--- Урок 2/Lesson2-Code2.py
+++
@@ -1,9 +0,0 @@
-def func(a, b):
-    if a == b:
-        return f'{a}'
-    elif a > b:
-        return f'{a}, {func(a - 1, b)}'
-    elif a < b:
-        return f'{a}, {func(a + 1, b)}'
-
-print(func(12, 10))
